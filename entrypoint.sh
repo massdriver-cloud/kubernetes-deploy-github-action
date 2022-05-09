e60de502773dc83877789b5c78c16351986596cc
Will <will@containerlabs.io>
2022-05-09 12:20:31 -0700

    outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# echo $ARTIFACT_KUBERNETES_CLUSTER  | base64 -d  > data.json
+echo $ARTIFACT_KUBERNETES_CLUSTER  | base64 -d  > data.json
 
 CERTIFICATE_AUTHORITY_DATA=$(jq '.authentication.cluster."certificate-authority-data"' data.json | sed s/\"//g)
 CLUSTER_SERVER=$(jq '.authentication.cluster.server' data.json | sed s/\"//g)
@@ -12,10 +12,6 @@ sed "s|<cluster-server>|${CLUSTER_SERVER}|" kube-config.tmp-0 > kube-config.tmp-
 sed "s/<user-token>/${TOKEN}/" kube-config.tmp-1 > kube-config
 
 rm kube-config.tmp-0 kube-config.tmp-1
-# set the env var for our custom file
-export KUBECONFIG=kube-config
-
-
-
 
 # outout the path to the kubeconfig file so downstream steps can use it.
+::set-output name=kube_config::kube-config
