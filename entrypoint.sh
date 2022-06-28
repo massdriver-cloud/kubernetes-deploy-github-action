2497ac9c0f4dbc835eb46408effea5812141aa53
Will <will@containerlabs.io>
2022-06-28 12:10:13 -0500

    updating notes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
+# quotes are SUPER REQUIRED here... without them the newlines don't get preserved
 echo "${ARTIFACT_KUBERNETES_CLUSTER}" > /kube-config
 echo "::set-output name=kube_config::/kube-config"
 export KUBECONFIG=/kube-config
-
-echo ${APPLICATION_ID}
-echo ${IMAGE}
-
-cat /kube-config | grep -v data | grep -v token
 
 # GITHUB Actions only interpolate yaml or bash-in-yaml
 # there's no way for this file to understant ${{ inputs.application_id }}
