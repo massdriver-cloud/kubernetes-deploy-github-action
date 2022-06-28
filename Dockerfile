1b5dcc4f10bef1238cb3fcfb26b7a8da29d73c2a
Will <will@containerlabs.io>
2022-06-28 11:17:12 -0500

    env vars only

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,7 @@ FROM alpine:latest
 RUN	apk add --no-cache \
   bash \
   ca-certificates \
-  curl \
-  jq
+  curl
 
 RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.21.0/bin/linux/amd64/kubectl && \
   chmod +x ./kubectl && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-echo $ARTIFACT_KUBERNETES_CLUSTER > kube-config
+# GITHUB Actions only interpolate yaml or bash-in-yaml
+# there's no way for this file to understant ${{ inputs.application_id }}
+# so we pass it in as an environment variable instead
 
-# output the path to the kubeconfig file so downstream steps can use it.
-echo "::set-output name=kube_config::kube-config"
-
-kubectl patch deployment ${{ inputs.application_id }} -p '{"spec": {"template": {"spec": {"containers": [{"name": "${{ inputs.application_id }}", "image": "${{ inputs.image }}"}]}}}}'
+kubectl patch deployment ${APPLICATION_ID} -p '{"spec": {"template": {"spec": {"containers": [{"name": "${APPLICATION_ID}", "image": "${IMAGE}"}]}}}}'
